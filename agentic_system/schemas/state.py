8aac2d453d38eace09b02a369046c556dcea20fc
Aayush Puri <aayushpuri2486@gmail.com>
2025-05-16 12:49:10 +0545

    Converted TypedDict to Pydantic BaseModel | Used Nested Pydantic models for state properties | use dots instead of brackets for access | cant use get method to return defaults

--- agentic_system/schemas/state.py
+++ agentic_system/schemas/state.py
@@ -2,16 +2,44 @@ from typing import List, Dict, Union
 from typing_extensions import TypedDict
 from pydantic import BaseModel
 
+class RelatedContent(BaseModel):
+    title: str
+    snippet: str
+
+class TrendItem(BaseModel):
+    keyword: str
+    relevance: int
+    related_content: List[RelatedContent]
+    related_queries: List[str]
+    trend_direction: str
+
 class Message(BaseModel):
     role: str
     content: str
 
-    class Config:
-        extra = 'forbid'  # Disallow additional fields
+    # class Config:
+    #     extra = 'forbid'  # Disallow additional fields
 
 class Step(BaseModel):
     step: str
     executed: bool
+
+    # class Config:
+    #     extra = 'forbid'
+
+class SearchItem(BaseModel):
+    title: str
+    content: str
+    url: str
+    score: float
+
+    # class Config:
+    #     extra = 'forbid'  # In case Tavily returns more fields
+
+class SearchResult(BaseModel):
+    term: str
+    results: List[SearchItem] 
+    # error: Union[str, None] 
 
     class Config:
         extra = 'forbid'
@@ -20,17 +48,17 @@ class CampaignState(BaseModel):
     messages: List[Message] 
     tone: str
     campaign_theme: str
-    target_audience: str = "General Audience"
-    duration_seconds: int = 30
-    trends: List[str]
-    search_results: List[Dict]
+    target_audience: str
+    duration_seconds: int 
+    trends: List[TrendItem]
+    search_results: List[SearchResult]
     hashtags: List[str]
     script: str
     current_step: str
     production_ideas: List[str]
     steps: List[Step]
 
-    class Config:
-        extra = "forbid"
+    # class Config:
+    #     extra = "forbid"
 
     
